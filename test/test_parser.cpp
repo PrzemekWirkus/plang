529ad1fdefcbbba096d144e2f1dcbe88aa791a11
PrzemekWirkus <przemyslaw.wirkus@arm.com>
2016-08-16 17:18:42 +0100

    Separated catch main from rest of code as manual suggests

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -1,16 +1,89 @@
 #include "catch.hpp"
 
 #include "plang_parser.h"
+#include <string>
 
-TEST_CASE( "Parse funcion (basic)", "[parser]") {
+TEST_CASE( "Parse @main function definition (empty body)", "[parser]" ) {
 
     // Simple @main funtcion with empty block
 
     PlangParser parser;
+    parser.load_input("@main() {}");
 
-    const char * str_main = "@main() {}";
-
-    parser.load_input(str_main);
-
-    REQUIRE( parser.parse_function() == 0);
+    REQUIRE(parser.parse_function() == 0);
 }
+
+TEST_CASE( "Parse functions definitions (broken)", "[parser]" ) {
+
+    {
+        PlangParser parser;
+        parser.load_input("@main() }");
+        REQUIRE(parser.parse_function() != 0);
+    }
+
+    {
+        PlangParser parser;
+        parser.load_input("@main( {}");
+        REQUIRE(parser.parse_function() != 0);
+    }
+
+    {
+        PlangParser parser;
+        parser.load_input("@main) {}");
+        REQUIRE(parser.parse_function() != 0);
+    }
+
+    {
+        PlangParser parser;
+        parser.load_input("@(){}");
+        REQUIRE(parser.parse_function() != 0);
+    }
+
+    {
+        PlangParser parser;
+        parser.load_input("@main {}");
+        REQUIRE(parser.parse_function() != 0);
+    }
+}
+
+TEST_CASE( "IF statement", "[parser]" ) {
+
+
+    {
+        PlangParser parser;
+        parser.load_input("@main() { if (id) {}  }");
+        REQUIRE(parser.parse_function() == 0);
+    }
+
+}
+
+
+TEST_CASE( "IF AS statement", "[parser]" ) {
+
+
+    {
+        PlangParser parser;
+        parser.load_input("@main() { if (id) as id {} }");
+        REQUIRE(parser.parse_function() == 0);
+    }
+
+}
+
+TEST_CASE( "block content", "[block]" ) {
+
+    {
+        PlangParser parser;
+        parser.load_input(R"(
+            @main() {
+            
+                if (long_name_of_variable) {
+                    
+                }
+            }
+        
+        )");
+        REQUIRE(parser.parse_function() == 0);
+    }
+
+}
+
---
+++ test/test_programs.cpp
@@ -0,0 +1,7 @@
+#include "catch.hpp"
+
+TEST_CASE( "", "[program]" ) {
+
+    REQUIRE( 1 == 1);
+
+}
